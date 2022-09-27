7f906a685905435540e3a70d9696ae100b2740a8
mehmetCelenk <95227625+mehmeCelenk@users.noreply.github.com>
2022-09-27 17:08:09 +0300

    Add files via upload

---
+++ inheritance/src/inheritance/PersonManager.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+public class PersonManager {
+	public void list() {
+		System.out.println("listelendi");
+	}
+	
+	public void add() {
+		System.out.println("Eklenddi");
+	}
+
+}
